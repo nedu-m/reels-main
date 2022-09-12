16956b973de422626299d289e3c18eb65f5698bf
nedu-m <davidsonchinedum@gmail.com>
2022-09-12 16:42:30 +0100

    fix: _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,8 +38,13 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta charSet="utf-8" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Outfit:wght@200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"

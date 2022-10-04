ba1aaabdf8fa5a598519b60782830324e59211f7
nedu-m <davidsonchinedum@gmail.com>
2022-10-04 15:22:58 +0100

    chore: folder update

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,32 @@
 // jest.config.js
-const nextJest = require('next/jest')
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./",
+});
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   // Add more setup options before each test is run
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>', '<rootDir>/pages', '<rootDir>/components'],
-  testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: [
+    "node_modules",
+    "<rootDir>",
+    "<rootDir>/pages",
+    "<rootDir>/components",
+  ],
+  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     "^@components(.*)$": "<rootDir>/components$1",
     "^@pages(.*)$": "<rootDir>/pages$1",
   },
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
-  transformIgnorePatterns: ['/node_modules/'],
-}
+  transformIgnorePatterns: ["/node_modules/"],
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
